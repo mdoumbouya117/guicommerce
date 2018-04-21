06660ff4e91c51235f329d619f4a920814c0108b
mdoumbouya117 <mdoumbouya117@gmail.com>
2018-04-21 21:25:22 +0200

    header home footer - init

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticlesNumberService } from '../shared/services/articles-number.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  public test: number;
+  public num: any;
+
+  constructor(public articlesNumberService: ArticlesNumberService) {
+    this.test = this.articlesNumberService.articlesNumber;
+    console.log(this.test);
+      }
+
+  ngOnInit() {
+    this.articlesNumberService.testNumberCount.subscribe(res => {
+      this.num = res;
+    });
+  }
+
+  increment() {
+    this.articlesNumberService.testNumberCount.subscribe(res => {
+      this.num = res;
+    });
+  }
+
+}
