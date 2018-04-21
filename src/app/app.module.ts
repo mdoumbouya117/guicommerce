d5a0f31492dc097246b7e0c121b1a9147eedff81
mdoumbouya117 <mdoumbouya117@gmail.com>
2018-04-21 21:49:21 +0200

    home page - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    CommonModule,
+    AppRoutingModule,
+    NgbModule.forRoot(),
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, HeaderComponent, HomeComponent, FooterComponent ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
