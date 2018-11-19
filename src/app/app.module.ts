b0051af32af8bb63a7be437b121e2332eb478bb9
Mohamed Doumbouya <mdoumbouya117@gmail.com>
2018-11-19 17:18:35 +0100

    fix- close issue - acces au component femme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HommeComponent } from './home/homme/homme.component';
 import { EnfantComponent } from './home/enfant/enfant.component';
+import { FemmeComponent } from './home/femme/femme.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
 
@@ -22,7 +23,7 @@ import { ArticleDetailComponent } from './articles/article-detail/article-detail
     BrowserModule
   ],
   providers: [],
-  declarations: [ AppComponent, HeaderComponent, HomeComponent, FooterComponent, SidebarComponent, HommeComponent, EnfantComponent, ArticlesComponent, ArticleDetailComponent ],
+  declarations: [ AppComponent, HeaderComponent, HomeComponent, FooterComponent, SidebarComponent, HommeComponent, EnfantComponent, FemmeComponent, ArticlesComponent, ArticleDetailComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
